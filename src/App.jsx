dada72b7ce5afb6f183fb839b20deb9a296188b5
molengeek <saoussan.elk@gmail.com>
2025-05-13 15:30:02 +0200

    index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
 import CardEnfant from './components/cardEnfant'
+import React, { useEffect, useState} from 'react';
 
-function App() {
+const App = () => {
+    const [timeframe, setTimeframe] = useState ("weekly")
 
-  return (
-    <>
-      <CardEnfant/>
-    </>
-  )
+    return (
+      <div className='app-container'>
+      
+      {data.map((item, index) => (
+        <CardEnfant
+          key={index}
+          title = {item.title}
+          hours = {item.hours}
+          choix = {item.choix}
+
+        />
+        ))}
+
+      </div>
+      )
 }
 
 export default App
